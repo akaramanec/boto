67bb07634495582c11c4086b26265ad1b321a07a
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-06 21:32:42 +0300

    try dix bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,33 +49,33 @@
 <div class="container">
     <div class="starter-template">
         <h1>Products</h1>
-
-        <div class="container">
-            @foreach($products as $product)
-                <div class="col-sm-6 col-md-3">
-                    <div class="thumbnail">
-                        <div class="labels">
-                        </div>
-                        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
-                        <div class="caption">
-                            <h3>iPhone X 64GB</h3>
-                            <p>71990 ₽</p>
-                            <p>
-                            <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
-                                <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                                <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"
-                                   class="btn btn-default"
-                                   role="button">Подробнее</a>
-                                <input type="hidden" name="_token" value="e1VXztfNgjhX3PGQfxAFxmYIfl77nnE8Zt2mdejx">
-                            </form>
-                            </p>
+        @if($products != null)
+            <div class="container">
+                @foreach($products as $product)
+                    <div class="col-sm-6 col-md-3">
+                        <div class="thumbnail">
+                            <div class="labels">
+                            </div>
+                            <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
+                            <div class="caption">
+                                <h3>iPhone X 64GB</h3>
+                                <p>71990 ₽</p>
+                                <p>
+                                <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
+                                    <button type="submit" class="btn btn-primary" role="button">В корзину</button>
+                                    <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"
+                                       class="btn btn-default"
+                                       role="button">Подробнее</a>
+                                    <input type="hidden" name="_token" value="e1VXztfNgjhX3PGQfxAFxmYIfl77nnE8Zt2mdejx">
+                                </form>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
-
-        {{ $products->links() }}
+                @endforeach
+            </div>
+            {{ $products->links() }}
+        @endif
     </div>
 </div>
 </body>
