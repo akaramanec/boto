bf4ea627b85a12a6db605c9208761f2325b0cecb
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-07 16:49:04 +0300

    try fix bot

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -36,10 +36,10 @@ class TestCommand extends Command
         $user = User::first();
         $this->replyWithMessage(['text' => 'User email in laravel: ' . $user->email]);
 
-        $response = \Telegram::getUpdates();
+//        $response = \Telegram::getUpdates();
 //        $text = sprintf('%s: %s' . PHP_EOL, 'Your chat namber', $telegramUser['from']['id']);
 //        $text .= sprintf('%s: %s' . PHP_EOL, 'Your name', $telegramUser['from']['username']);
 
-        $this->replyWithMessage(['text' => $response]);
+//        $this->replyWithMessage(['text' => $response]);
     }
 }
