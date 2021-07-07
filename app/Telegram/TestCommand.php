cf64a346fbe1e639bb29c67c03b1410644af7dde
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-07 18:12:27 +0300

    try fix bot

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -35,16 +35,16 @@ class TestCommand extends Command
         $this->replyWithChatAction(['action' => Actions::TYPING]);
         $user = User::first();
         $this->replyWithMessage(['text' => 'User email in laravel: ' . $user->email]);
-
-//        $response = \Telegram::getUpdates();
+        $response = \Telegram::removeWebhook();
 
 //        $botId = $response->getId();
 //        $firstName = $response->getFirstName();
 //        $username = $response->getUsername();
 //        $response = \Telegram::getUpdates();
+//        $telegramUser = $response['message'];
 //        $text = sprintf('%s: %s' . PHP_EOL, 'Your chat namber', $telegramUser['from']['id']);
 //        $text .= sprintf('%s: %s' . PHP_EOL, 'Your name', $telegramUser['from']['username']);
-
+//        $this->replyWithMessage(compact('text'));
 //        $this->replyWithMessage(['text' => '$botId: ' . $botId . '; $firstName: ' . $firstName . '; $username: ' . $username]);
     }
 }
