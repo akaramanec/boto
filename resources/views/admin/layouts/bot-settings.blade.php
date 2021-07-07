21a8dcbc416424bf1b98462742047dd52a3306cd
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-07 14:29:50 +0300

    add bot settings

--- resources/views/admin/layouts/bot-settings.blade.php
+++ resources/views/admin/layouts/bot-settings.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Registered bot messages
+            Bot settings
         </h2>
     </x-slot>
     @yield('content')
