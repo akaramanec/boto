bbfc4fc0df86ca4a5ff13a296b32cbec3879315b
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-07 19:13:02 +0300

    try fix bot

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -32,7 +32,8 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        $telegramUser = \Telegram::getUpdates()[0]['message'];
+        $telegramUser = \Telegram::getWebhookUpdates();
+        dd($telegramUser);
         $text = sprintf('%s: %s' . PHP_EOL, 'Your chat namber', $telegramUser['from']['id']);
         $text .= sprintf('%s: %s' . PHP_EOL, 'Your name', $telegramUser['from']['username']);
 
