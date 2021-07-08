54c2c5a6c32492290c0a95460c5b4949e49c8b6e
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-08 11:16:39 +0300

    test command answers

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -19,7 +19,7 @@ class TelegramController extends Controller
                 TelegramUser::create($telegramMessage['from']);
             }
         } else {
-            Log::error('not message', Telegram::bot()->getWebhookUpdate()->toArray());
+            Log::error('not message', ['data' => Telegram::bot()->getWebhookUpdate()->toArray()['data']]);
         }
 //
 //        if (isset($telegramMessage['text'])) {
