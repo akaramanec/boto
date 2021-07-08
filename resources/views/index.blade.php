64879b3cba645199bb5167a2f1621ce8b44adf5f
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-09 02:51:17 +0300

    add file upload

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,19 +56,12 @@
                         <div class="thumbnail">
                             <div class="labels">
                             </div>
-                            <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
+                            <img src="{{ $product->image }}" alt="{{ $product->name }}">
                             <div class="caption">
-                                <h3>iPhone X 64GB</h3>
-                                <p>71990 ₽</p>
-                                <p>
-                                <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
-                                    <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                                    <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"
-                                       class="btn btn-default"
-                                       role="button">Подробнее</a>
-                                    <input type="hidden" name="_token" value="e1VXztfNgjhX3PGQfxAFxmYIfl77nnE8Zt2mdejx">
-                                </form>
-                                </p>
+                                <h3>{{ $product->name }}</h3>
+                                <p>{{ $product->price }} UHR</p>
+                                <p>{{ $product->description }}</p>
+                                @if($product->availability) <p> <b> available </b> @else <del> not available</del> @endif</p>
                             </div>
                         </div>
                     </div>
