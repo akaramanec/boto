a3778323e89e0b9406fe48010d4bf977ac4d6674
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-09 11:44:26 +0300

    add buy to product

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -18,6 +18,6 @@ class Order extends Model
 
     public function user()
     {
-        return $this->belongsTo(TelegramUser::class, 'user_id', 'id');
+        return $this->belongsTo(TelegramUser::class);
     }
 }
