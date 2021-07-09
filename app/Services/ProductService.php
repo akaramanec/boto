5e717a9d59a5e1ea25847ed2b74cf30cf6675577
Alexandr Karamanec <akaramanec@gmail.com>
2021-07-09 05:13:47 +0300

    fix keyboard buttons

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -12,16 +12,22 @@ class ProductService extends ProductRepository
     public const BUTTON_PREV = 'prev';
     public const BUTTON_NEXT = 'next';
 
+    protected $buttonTexts = [
+        self::BUTTON_BUY => 'Buy',
+        self::BUTTON_PREV => '<< Prev',
+        self::BUTTON_NEXT => 'Next >>',
+    ]
+
     protected $buttons = [];
 
-    public function getProductButtons(Eloquent $product): array
+    public function getProductButtons(Eloquent $product)
     {
         $row = 0;
         if($product->availability) {
-            $this->buttons[$row][] = [self::BUTTON_BUY];
+            $this->buttons[$row][] = ['text' => $this->buttonTexts[self::BUTTON_BUY], 'callback_data' => self::BUTTON_BUY];
         }
-        $this->buttons[$row][] = [self::BUTTON_PREV];
-        $this->buttons[$row][] = [self::BUTTON_NEXT];
+        $this->buttons[$row][] = ['text' => $this->buttonTexts[self::BUTTON_PREV], 'callback_data' => self::BUTTON_PREV];
+        $this->buttons[$row][] = ['text' => $this->buttonTexts[self::BUTTON_NEXT], 'callback_data' => self::BUTTON_NEXT];
         return $this->buttons;
     }
 }
